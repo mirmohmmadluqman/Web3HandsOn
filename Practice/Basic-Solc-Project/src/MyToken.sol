67c0f3cac823f4db5a282d31fb6797cb4746201d
Mir Mohmmad Luqman <mirmohmmadluqman@gmail.com>
2026-02-03 21:34:32 +0530

    Revising Security

---
+++ Practice/Basic-Solc-Project/src/MyToken.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.33;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MyToken is ERC20 {
+    constructor() ERC20("MyToken", "MTK") {
+        
+    }
+}
